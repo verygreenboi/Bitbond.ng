e1416e55f3a7017cea01c6194b00deb74fbaf77f
Thompson Edolo <verygreenboi@live.com>
2017-01-28 11:46:00 +0100

    Fix Gzip

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var gzipStatic = require('connect-gzip-static');
+var oneDay = 86400000;
 
 /* GET home page. */
-router.use('/', express.static(path.join(__dirname, '../dist')));
+router.use('/', gzipStatic(path.join(__dirname, '../dist')));
 
 module.exports = router;
